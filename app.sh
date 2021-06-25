2cdb93ed5f7c6d98c85a98c16752f4486ee3bf1b
razaqultegar <myrazaqul@gmail.com>
2021-06-25 10:31:28 +0700

    Update installation apps bundle scripts

---
+++ app.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo 'Install alfred'
+echo '------------------'
+brew install --cask alfred
+
+echo 'Install anydesk'
+echo '------------------'
+brew install --cask anydesk
+
+echo 'Install dropbox'
+echo '------------------'
+brew install --cask dropbox
+
+echo 'Install figma'
+echo '------------------'
+brew install --cask figma
+
+echo 'Install firefox'
+echo '------------------'
+brew install --cask firefox
+
+echo 'Install postman'
+echo '------------------'
+brew install --cask postman
+
+echo 'Install rectangle'
+echo '------------------'
+brew install --cask rectangle
+
+echo 'Install slack'
+echo '------------------'
+brew install --cask slack
+
+echo 'Install tableplus'
+echo '------------------'
+brew install --cask tableplus
+
+echo 'Install telegram-desktop'
+echo '------------------'
+brew install --cask telegram-desktop
+
+echo 'Install the-unarchiver'
+echo '------------------'
+brew install --cask the-unarchiver
+
+echo 'Install visual-studio-code'
+echo '------------------'
+brew install --cask visual-studio-code

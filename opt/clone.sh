bb35599537adb0716137c6a3dc9dede103522c2e
razaqultegar <myrazaqul@gmail.com>
2021-07-01 00:48:39 +0700

    Add clone.sh

---
+++ opt/clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 'Create directory'
+mkdir "${HOME}/GitHub"
+mkdir "${HOME}/Sites"
+
+echo 'Cloning repositories...'
+# Personal
+git clone git@github.com:razaqultegar/razaqultegar.com.git $HOME/GitHub/razaqultegar.com
+git clone git@github.com:razaqultegar/notebook.git $HOME/GitHub/notebook
+
+# Office
+git clone git@github.com:razaqultegar/limbodigital-site.git $HOME/GitHub/limbodigital-site.git
+git clone git@gitlab.com:biker-shop/biker-shop-server.git $HOME/Sites/biker-shop-server
+git clone git@gitlab.com:limboproject/babershop.git $HOME/Sites/babershop

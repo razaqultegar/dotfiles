68a364fa41f9d725a72212b0d111a71ce50fa4e8
razaqultegar <myrazaqul@gmail.com>
2022-02-19 22:34:15 +0700

    Update app.sh

--- opt/app.sh
+++ opt/app.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 
-echo 'Install dropbox'
-echo '------------------'
+brew install --cask dbngin
 brew install --cask dropbox
-
-echo 'Install firefox'
-echo '------------------'
 brew install --cask firefox
-
-echo 'Install postman'
-echo '------------------'
-brew install --cask postman
-
-echo 'Install rectangle'
-echo '------------------'
+brew install --cask imageoptim
 brew install --cask rectangle
-
-echo 'Install tableplus'
-echo '------------------'
-brew install --cask tableplus
-
-echo 'Install telegram-desktop'
-echo '------------------'
 brew install --cask telegram-desktop
-
-echo 'Install the-unarchiver'
-echo '------------------'
 brew install --cask the-unarchiver
-
-echo 'Install visual-studio-code'
-echo '------------------'
 brew install --cask visual-studio-code
+brew install --cask vlc

8e70b7e5b94e554262214268640d7414dba9aa7d
razaqultegar <myrazaqul@gmail.com>
2022-02-19 22:33:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,73 +1,71 @@
 #!/bin/bash
 
-echo 'Setting up your Mac...'
+echo "Setting up your Mac..."
 
 # Hide "last login" line when starting a new terminal session
 touch $HOME/.hushlogin
 
 # Check for Oh My Zsh and install if we don't have it
-echo 'Checking oh-my-zsh'
 if test ! $(which omz); then
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 fi
 
 # Check for Homebrew and install if we don't have it
-echo 'Checking homebrew'
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-# Create a directory for global my project, this optional
-mkdir "${HOME}/Developments"
-mkdir "${HOME}/GitHub"
-mkdir "${HOME}/Sites"
+# Removes .zshrc folders (if it exists) and symlinks zsh prefs
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
-# Installing required packages
+# Installing binaries packages
+brew install bash
+brew install coreutils
+brew install ffmpeg
+brew install git
+brew install httpie
+
+# Installing development packages
+brew install imagemagick
+brew install php@7.4
+brew install composer
 brew install mysql
-brew services start mysql
 brew install node
 brew install yarn
-brew install php@7.4
-
-# Installing Composer
-EXPECTED_COMPOSER_CHECKSUM="$(curl https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_COMPOSER_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-if [ "$EXPECTED_COMPOSER_CHECKSUM" != "$ACTUAL_COMPOSER_CHECKSUM" ]
-then
-  >&2 echo 'ERROR: Invalid installer checksum'
-  rm composer-setup.php
-  exit 1
-fi
-php composer-setup.php
-rm composer-setup.php
-mv composer.phar /usr/local/bin/composer
-
-# Installing Laravel
-composer global require laravel/installer
-
-# Symlink zsh prefs
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/shell/.zshrc $HOME/.zshrc
-
-# Add global .gitignore
-ln -s $HOME/.dotfiles/shell/.gitignore_global $HOME/.gitignore_global
-git config --global core.excludesfile $HOME/.gitignore_global
 
 # Set default MySQL root password and auth type
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
-# Create a directory for global packages and tell npm where to store globally installed packages
+# Install global Composer packages
+/usr/local/bin/composer global require laravel/installer
+
+# Create a directory for global my project (this optional)
 mkdir "${HOME}/.npm-packages"
+mkdir "${HOME}/Development"
+mkdir "${HOME}/GitHub"
+mkdir "${HOME}/Sites"
+
+# Create github subdirectories
+mkdir "${HOME}/GitHub/Personal"
+mkdir "${HOME}/GitHub/Work"
+
+# Configure npm
 npm config set prefix "${HOME}/.npm-packages"
 
-echo 'Install required applications'
-echo 'This will take a long time. Are you sure you want to to this? (y/n) '
-read -p 'Answer: '  reply
+# Add global .gitignore
+ln -s $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
+git config --global core.excludesfile $HOME/.gitignore_global
 
-if [[ $reply =~ ^[Yy]$ ]]
-then
+echo "Install required applications"
+echo "This will take a long time. Are you sure you want to to this? (y/n)"
+read -p "Answer: "  reply
+
+if [[ $reply =~ ^[Yy]$ ]] then
   source ~/.dotfiles/opt/app.sh
 fi
 
-echo 'All done! Please restart your computer to finalize the process'
+echo "All done! Please restart your computer to finalize the process :)"
